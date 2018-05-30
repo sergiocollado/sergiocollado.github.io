230bfe8aac701213ce2cd2c969fe0f8f9bb6bc8b
Sergio González Collado <sergio.collado@gmail.com>
2018-05-30 23:45:25 +0200

    Update pelotas.js

--- pelotas.js
+++ pelotas.js
@@ -1,5 +1,16 @@
 window.onload = function() {
 
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
 
+var width = canvas.width = window.innerWidth;
+var height = canvas.height = window.innerHeight;
+
+// function to generate random number
+
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+}
 
 }
